ad42edecbee0b60f263610bbb0dfb8bf851538d4
christynatalia <57931555+christynatalia@users.noreply.github.com>
2021-05-10 17:02:09 +0700

    connected with the mongodb and created the custom middleware

---
+++ server/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const mongoose =require('mongoose')
+const port = 4000
+const {MONGOURI} = require('./keys')
+
+require('./models/users')
+
+const customMiddleware = (req,res,next)=> {
+    console.log("middleware executed")
+    next()
+}
+
+
+//It is used to all page
+//app.use(customMiddleware)
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+
+mongoose.connect(MONGOURI)
+mongoose.connection.on('connected',()=> {
+    console.log("connected to mongo")
+} )
+
+mongoose.connection.on('error',(err)=> {
+    console.log("error", err)
+} )
+
+app.get('/', (req,res) => {
+    console.log("home page")
+    res.send("Hello world")
+})
+
+//specific page middleware 
+app.get('/about', customMiddleware, (req,res) => {
+    console.log("about page")
+    res.send("About")
+})
+
+app.listen(port, ()=> {
+    console.log("server is running on", port)
+})
---
+++ server/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI:"mongodb+srv://christynj:rGrLGK4DyYArLFcF@cluster0.zq7fx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
