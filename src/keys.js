781b2c5fbc9c3e267a706dbdc5b2191a78ed761d
alviancode <alvianwijaya200@gmail.com>
2021-05-23 23:47:40 +0700

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
 module.exports={
-    CLOUD_URI:process.env.CLOUD_URI,
+    CLOUD_URI:"https://api.cloudinary.com/v1_1/redgram/image/upload",
     API_URL:process.env.API_URL
 }
