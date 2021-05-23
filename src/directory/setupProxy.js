203f1414c094728a9caeb0b6ff4926a136ab0849
alviancode <alvianwijaya200@gmail.com>
2021-05-23 22:33:34 +0700

    updates

--- src/directory/setupProxy.js
+++ src/directory/setupProxy.js
@@ -1,7 +1,8 @@
 const proxy = require("http-proxy-middleware");
+const { API_URL } = require('../../keys')
 
 module.exports = function(app) {
   app.use(
-    proxy(["/api", , "/otherApi"], { target: "https://redgram-api.herokuapp.com" })
+    proxy(["/api", , "/otherApi"], { target: API_URL })
   );
 };
--- src/keys.js
+++ src/keys.js
@@ -2,4 +2,5 @@ require('dotenv').config()
 
 module.exports={
     CLOUD_URI:process.env.CLOUD_URI,
+    API_URL:process.env.API_URL
 }
