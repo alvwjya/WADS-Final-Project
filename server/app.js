b6ed87064d4ed57ca3b7f2d0bc8f6cf6e1f31c84
alviancode <alvianwijaya200@gmail.com>
2021-05-23 21:10:07 +0700

    testing

--- server/app.js
+++ server/app.js
@@ -2,17 +2,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 4000;
-//const { MONGOURI } = require('./keys');
+const { MONGOURI } = process.env.MONGODB_URI || require('./keys');
 
-/*
+
 const customMiddleware = (req, res, next) => {
     console.log("middleware executed");
     next();
 }
 
-
-//It is used to all page
-//app.use(customMiddleware)
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -28,7 +25,6 @@ mongoose.connection.on('error', (err) => {
 })
 
 
-
 app.use(express.json())
 require('./models/users');
 require('./models/post');
@@ -37,18 +33,16 @@ app.use(require('./routes/post'));
 app.use(require('./routes/user'));
 app.use(require('./routes/search'));
 
-*/
 
 app.get('/', (req, res) => {
     console.log("Welcome to Redgram");
     res.send("Hello World!!!");
 })
-/*
+
 app.get('/about', customMiddleware, (req, res) => {
     console.log("about page");
     res.send("About");
 })
-*/
 
 app.listen(port, () => {
     console.log("Server is running on port: ", port);
