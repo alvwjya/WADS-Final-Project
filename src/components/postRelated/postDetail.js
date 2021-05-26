a524a7bc505fb7f46abe42c59d1fa645f30f9d03
alviancode <alvianwijaya200@gmail.com>
2021-05-26 19:14:51 +0700

    testing

--- src/components/postRelated/postDetail.js
+++ src/components/postRelated/postDetail.js
@@ -270,19 +270,9 @@ function PostDetail({ match }) {
                                             <a href={`/profile/${record.username._id}`} className="streched-link text-light"><strong>{record.username.username}</strong></a>
                                             <text>{record.comment}</text>
                                         </div>
-
-
                                     )
                                 }
-
                             })
-                        }
-                        {/*comments.username === JSON.parse(localStorage.getItem("user"))._id
-                            &&
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                                <rect className="btn" x="0" y="0" width="20" height="20" onClick={() => { deletePost(item._id) }}></rect>
-                            </svg>*/
                         }
                     </div>
                 </div>
